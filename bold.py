395ea6ed9e5633e10947a2edae73077ebc83c0e3
akedidi <kedidi.anis@gmail.com>
2025-11-27 15:56:29 +0100

    Refactor bold.py to format table rows with XML

--- bold.py
+++ bold.py
@@ -1,35 +1,32 @@
-for i, val in enumerate(cells):
-    text = xml_escape(val)
+for row in recap:
+    cells = [
+        row["Label"],
+        row["Samples"],
+        int(row["Average (ms)"]),
+        int(row["Min (ms)"]),
+        int(row["Max (ms)"]),
+        row["Std Dev (ms)"],
+        f"{row['Error %']:.2f}%",
+        row["Throughput (/min)"],
+        row["Received KB/sec"],
+        row["Sent KB/sec"],
+        row["Avg Bytes"],
+    ]
 
-    # 🔥 bold uniquement si première colonne (i == 0)
-    if i == 0:
-        data_rows_xml += f"""
-        <w:tc>
-          <w:tcPr/>
-          <w:p>
-            <w:r>
-              <w:rPr>
-                <w:b/>                      <!-- Gras -->
-                <w:sz w:val="16"/>
-                <w:szCs w:val="16"/>
-              </w:rPr>
-              <w:t>{text}</w:t>
-            </w:r>
-          </w:p>
-        </w:tc>
-        """
-    else:
-        data_rows_xml += f"""
-        <w:tc>
-          <w:tcPr/>
-          <w:p>
-            <w:r>
-              <w:rPr>
-                <w:sz w:val="16"/>
-                <w:szCs w:val="16"/>
-              </w:rPr>
-              <w:t>{text}</w:t>
-            </w:r>
-          </w:p>
-        </w:tc>
-        """
+    table_xml += "<w:tr>"
+
+    for i, val in enumerate(cells):
+        text = xml_escape(str(val))  # ESCAPE OBLIGATOIRE
+
+        if i == 0:   # 🔥 première colonne bold
+            table_xml += f"""
+            <w:tc><w:p><w:r><w:rPr><w:b/><w:sz w:val="16"/><w:szCs w:val="16"/></w:rPr>
+            <w:t>{text}</w:t></w:r></w:p></w:tc>
+            """
+        else:        # colonnes normales
+            table_xml += f"""
+            <w:tc><w:p><w:r><w:rPr><w:sz w:val="16"/><w:szCs w:val="16"/></w:rPr>
+            <w:t>{text}</w:t></w:r></w:p></w:tc>
+            """
+
+    table_xml += "</w:tr>"
